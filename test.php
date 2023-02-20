1642fd75be2212f8995c66e2ff2db6c4e38d8ea0
Damien Bin <damien.bin@afpa.fr>
2023-02-20 13:22:49 +0100

    v3bis - test3.php

--- test.php
+++ test.php
@@ -1,9 +1,17 @@
 <?
     $age = 10;
+    $genre = "H";
     
     if ($age < 18) {
         echo "Vous êtes mineur";
     } else {
         echo "Vous êtes majeur";
     }
+
+    if ($genre === "H") {
+        echo "Vous êtes un homme";
+    } else {
+        echo "Vous êtes une femme";
+    }
+
 ?>
